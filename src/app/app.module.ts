9b4d689da8fb7c5897896957c00f6861ad1d4304
meganmuirhead <megan@s56.net>
2018-06-13 19:54:08 -0600

    html redone

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import {MatListModule, MatTableModule} from '@angular/material';
 import {AngularFireModule} from 'angularfire2';
 import {environment} from '../environments/environment';
 import {AngularFirestoreModule} from 'angularfire2/firestore';
+import { PlayersComponent } from './players/players.component';
 
 
 
@@ -21,7 +22,8 @@ import {AngularFirestoreModule} from 'angularfire2/firestore';
   declarations: [
     AppComponent,
     ScoreCardComponent,
-    HomePageComponent
+    HomePageComponent,
+    PlayersComponent
   ],
   imports: [
     BrowserModule,
--- src/app/score-card/score-card.component.ts
+++ src/app/score-card/score-card.component.ts
@@ -7,23 +7,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./score-card.component.less']
 })
 export class ScoreCardComponent implements OnInit {
-  lvls = [
-    {value: 'pro-0', viewValue: 'Pro'},
-    {value: 'champ-1', viewValue: 'Champion'},
-    {value: 'men-2', viewValue: 'Men'},
-    {value: 'women-3', viewValue: 'Women'}
-  ];
-  pars = [
-    {value: 'hole1', viewValue: 'Hole 1'},
-    {value: 'hole2', viewValue: 'Hole 2'},
-    {value: 'hole3', viewValue: 'Hole 3'},
-    {value: 'hole4', viewValue: 'Hole 4'},
-    {value: 'hole5', viewValue: 'Hole 5'},
-    {value: 'hole6', viewValue: 'Hole 6'},
-    {value: 'hole7', viewValue: 'Hole 7'},
-    {value: 'hole8', viewValue: 'Hole 8'},
-    {value: 'hole9', viewValue: 'Hole 9'},
-  ];
+  currentList = [];
   constructor() { }
 
   ngOnInit() {
